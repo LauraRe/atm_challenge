9a9806e9a8ad44c44eb262a6a25dbeab3f55f9b3
Alecia <aleciabenjamin@gmail.com>
2019-01-22 20:48:11 +0100

    update user stories and create expected output for expired card

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,14 +1,10 @@
 require './lib/atm.rb'
 require 'date'
 describe Atm do
-    let(:account) { instance_double('Account', pin_code: '1234') }
+    let(:account) { instance_double('Account', pin_code: '1234', exp_date: '04/20') }
 
     before do
-    # Before each test we need to add an attribute of `balance`
-    # to the `account` object and set the value to `100`
         allow(account).to receive(:balance).and_return(100)
-    # We also need to allow `account` to receive the new balance
-    # using the setter method `balance=`
         allow(account).to receive(:balance=)
     end
 
@@ -40,6 +36,12 @@ describe Atm do
     it 'rejects withdraw if pin is wrong' do
         expected_output = { status: false, message: 'wrong pin', date: Date.today }
         expect(subject.withdraw(50, 9999, account)).to eq expected_output
+    end
+    
+    it 'reject withdraw if card is expired' do
+        allow(account).to receive(:exp_date).and_return('12/15')
+        expected_output = { status: false, message: 'card expired', date: Date.today }
+        expect(subject.withdraw(6, '1234', account)).to eq expected_output
     end
 
 end
