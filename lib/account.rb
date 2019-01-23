67734fb2e547bba50a37817bc690c81daf06fb67
Laura Reale <laaru1988@gmail.com>
2019-01-23 20:12:19 +0100

    update README.md, create account.rb and account_spec.rb files, define Account class

---
+++ lib/account.rb
@@ -0,0 +1,12 @@
+class Account
+    attr_accessor :pin_code
+
+    def initialize
+        @account_number = account_number
+        @pin_code = pin_code
+        @exp_date = exp_date
+        @account_status = account_status
+        @balance = balance
+    end
+end
+
