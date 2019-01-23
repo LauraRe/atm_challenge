635dca186ff9867487ea2ce262d5fd046eb83cdc
Alecia <aleciabenjamin@gmail.com>
2019-01-23 21:19:53 +0100

    create test and implement code to verify 4 digit pin

--- lib/account.rb
+++ lib/account.rb
@@ -3,10 +3,19 @@ class Account
 
     def initialize
         @account_number = account_number
-        @pin_code = pin_code
+        @pin_code = rand(1000..9999)
         @exp_date = exp_date
         @account_status = account_status
         @balance = balance
     end
+
+    def pin_code_length
+        number_length = Math.log10(@pin_code).to_i + 1
+        
+    end
+
+        
+
+
 end
 
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,4 +2,10 @@ require './lib/account.rb'
 
 describe Account do
 
+    it 'check length of a number' do
+        number = 1234
+        number_length = Math.log10(number).to_i + 1
+        expect(number_length).to eq 4
+    end
+
 end
