8f4998f85893e863988cc35f44e67c95cb8a311c
Laura Reale <laaru1988@gmail.com>
2019-01-24 13:52:08 +0100

    test if account has owner and raise error if no owner is found

--- lib/account.rb
+++ lib/account.rb
@@ -1,31 +1,28 @@
 class Account
     STANDARD_VALIDITY_YRS = 5
-    attr_accessor :pin_code, :balance, :account_status, :exp_date
+    attr_accessor :pin_code, :balance, :account_status, :exp_date, :owner
     
-    def initialize
+    def initialize(attrs = {})
         @pin_code = rand(1000..9999)
         @balance = balance
         @exp_date = Date.today.next_year(STANDARD_VALIDITY_YRS).strftime('%m/%Y')
         @account_status = :active
-        
+        set_owner(attrs[:owner])
     end
 
     def deactivate
         @account_status = :deactivated
     end
 
-
-    
+    private
+        def set_owner(obj)
+            obj == nil ? missing_owner : @owner = obj
+        end
         
-   
+        def missing_owner
+            raise "An Account owner is required"
+        end
     
-
-
-
-
-    
-
-
 
 end
 
