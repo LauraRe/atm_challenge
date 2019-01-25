150e323d13a0704c14331b9a3e06b44d15639554
Laura Reale <laaru1988@gmail.com>
2019-01-25 11:56:09 +0100

    restructure specs in person_spec file, create withdraw funds method testing for missing account and/or atm and increase cash when atm response is true

--- lib/person.rb
+++ lib/person.rb
@@ -14,18 +14,14 @@ class Person
     def create_account
         @account = Account.new(owner: self, balance: 0)
     end
-    
-    def set_name(name)
-        if name == nil then
-            raise ArgumentError.new('A name is required')
-        else
-            name
-        end
-    end
 
     def deposit(amount)
         @account == nil ? missing_account : deposit_funds(amount)
       end
+
+    def withdraw(args = {})
+        @account == nil ? missing_account : withdraw_funds(args)
+    end
 
     private
 
@@ -33,10 +29,30 @@ class Person
         @cash -= amount
         @account.balance += amount
     end
+
+    def withdraw_funds(args)
+        args[:atm] == nil ? missing_atm : atm = args[:atm]
+        account = @account
+        amount = args[:amount]
+        pin = args[:pin]
+        response = atm.withdraw(amount, pin, account)
+        response[:status] == true ? increase_cash(response) : response
+    end
+
+    def increase_cash(response)
+        @cash += response[:amount]
+    end
+
+    def set_name(name)
+        if name == nil then
+            raise ArgumentError.new('A name is required')
+        else
+            name
+        end
+    end
     
     def missing_account
         raise RuntimeError, 'No account present'
     end
-
 
 end
